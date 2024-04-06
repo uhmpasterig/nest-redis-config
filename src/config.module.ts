44d82756b3acaad2ea64434c1f41157881732e93
Uhmpasterig <805luca805@gmail.com>
2024-04-06 13:40:55 +0200

    fix: remove unused module creation methods

--- src/config.module.ts
+++ src/config.module.ts
@@ -1,27 +1,7 @@
 import { DynamicModule, Global, Module } from "@nestjs/common";
 import { ConfigService } from "./config.service";
-import {
-  ClientProviderOptions,
-  ClientsModule,
-  Transport,
-} from "@nestjs/microservices";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 import { CONFIG_SERVICE_PORT, CONFIG_SERVICE_NAME } from "./config.setup";
-
-const createConfigServiceOptions = (port: number): ClientProviderOptions => ({
-  name: CONFIG_SERVICE_NAME,
-  transport: Transport.TCP,
-  options: {
-    port,
-  },
-});
-
-const createConfigServiceClient = (port: number) =>
-  ClientsModule.registerAsync([
-    {
-      name: CONFIG_SERVICE_NAME,
-      useFactory: () => createConfigServiceOptions(port),
-    },
-  ]);
 
 @Global()
 @Module({})
@@ -29,49 +9,31 @@ export class ConfigModule {
   private static createDynamicModule(configServicePort: number): DynamicModule {
     return {
       module: ConfigModule,
-      exports: [ConfigService],
       providers: [ConfigService],
-      imports: [createConfigServiceClient(configServicePort)],
+      exports: [ConfigService],
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name: CONFIG_SERVICE_NAME,
+            useFactory: () => ({
+              transport: Transport.TCP,
+              options: { port: configServicePort },
+            }),
+          },
+        ]),
+      ],
     };
   }
 
-  private static createFeatureModule(): DynamicModule {
-    return {
-      module: ConfigModule,
-      exports: [ConfigService],
-      providers: [ConfigService],
-    };
-  }
-
-  static forRoot(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
-    return this.createDynamicModule(configServicePort);
-  }
-
-  static forRootAsync(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
-    return this.createDynamicModule(configServicePort);
-  }
-
-  static register(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
-    return this.createDynamicModule(configServicePort);
-  }
-
-  static registerAsync(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
+  static forRoot(configServicePort: number = CONFIG_SERVICE_PORT): DynamicModule {
     return this.createDynamicModule(configServicePort);
   }
 
   static forFeature(): DynamicModule {
-    return this.createFeatureModule();
-  }
-
-  static forFeatureAsync(): DynamicModule {
-    return this.createFeatureModule();
+    return {
+      module: ConfigModule,
+      providers: [ConfigService],
+      exports: [ConfigService],
+    };
   }
 }
